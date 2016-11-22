1be1f1feb39b4b9e7127f85cdde05f071e0dfe7d
SANTANU <santanubera.cse@gmail.com>
2016-11-22 18:59:08 +0530

    My Update

--- Box Game/Form1.Designer.vb
+++ Box Game/Form1.Designer.vb
@@ -24,8 +24,7 @@ Partial Class MainForm
     Private Sub InitializeComponent()
         Me.Show_Hide = New System.Windows.Forms.Button()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txt2 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -48,31 +47,21 @@ Partial Class MainForm
         Me.PictureBox1.TabIndex = 1
         Me.PictureBox1.TabStop = False
         '
-        'Label1
+        'txt2
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(70, 139)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(66, 13)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Picture Here"
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(171, 279)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(113, 32)
-        Me.Button1.TabIndex = 3
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.txt2.AutoSize = True
+        Me.txt2.Location = New System.Drawing.Point(70, 139)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(66, 13)
+        Me.txt2.TabIndex = 2
+        Me.txt2.Text = "Picture Here"
         '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(536, 454)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt2)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Show_Hide)
         Me.Name = "MainForm"
@@ -85,6 +74,5 @@ Partial Class MainForm
 
     Friend WithEvents Show_Hide As Button
     Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Button1 As Button
+    Friend WithEvents txt2 As Label
 End Class
