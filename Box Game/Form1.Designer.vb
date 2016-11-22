b178d489949682b51195454a09bcd061da97e5f6
SANTANU <santanubera.cse@gmail.com>
2016-11-22 18:35:43 +0530

    My CHange 2

--- Box Game/Form1.Designer.vb
+++ Box Game/Form1.Designer.vb
@@ -25,6 +25,7 @@ Partial Class MainForm
         Me.Show_Hide = New System.Windows.Forms.Button()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -56,11 +57,21 @@ Partial Class MainForm
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Picture Here"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(171, 279)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(113, 32)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(536, 454)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Show_Hide)
@@ -75,4 +86,5 @@ Partial Class MainForm
     Friend WithEvents Show_Hide As Button
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
 End Class
