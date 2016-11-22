822c62264b5d7bc99fdafec6053260a8d35c797d
satyadipan <satyadipan2014@gmail.com>
2016-11-22 10:10:24 +0530

    First Try

--- Box Game/Form1.Designer.vb
+++ Box Game/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class MainForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,44 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Show_Hide = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Show_Hide
+        '
+        Me.Show_Hide.Location = New System.Drawing.Point(290, 272)
+        Me.Show_Hide.Name = "Show_Hide"
+        Me.Show_Hide.Size = New System.Drawing.Size(138, 47)
+        Me.Show_Hide.TabIndex = 0
+        Me.Show_Hide.Text = "Show"
+        Me.Show_Hide.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Box_Game.My.Resources.Resources.Edit1
+        Me.PictureBox1.Location = New System.Drawing.Point(226, 63)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(160, 147)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(536, 454)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Show_Hide)
+        Me.Name = "MainForm"
+        Me.Text = "Main"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Show_Hide As Button
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- Box Game/My Project/Application.Designer.vb
+++ Box Game/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Box_Game.Form1
+            Me.MainForm = Global.Box_Game.MainForm
         End Sub
     End Class
 End Namespace
